8712f2931adf31bc8b49c172099115292891ce01
Cusican33 <Yariel.Perez66@gmail.com>
2026-05-04 18:14:04 -0400

    progress

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class BinarySearch {
+
+    // implements binary search on the elements list for target,
+    // and returns the index at which the target is found, or -1 if
+    public static int binarySearch(int[] elements, int target)
+    {
+
+        int loopCounter = 0; // for testing
+        int leftIdx = 0;
+        int rightIdx = elements.length - 1;
+
+        while (rightIdx - leftIdx > 1) {  // TODO: determine this condition (hint: see slides 61-63)
+            loopCounter++; // for testing
+            System.out.print(loopCounter + " "); // for testing
+            int middleIdx = (rightIdx - leftIdx) / 2 + leftIdx;
+            if (elements[middleIdx] > target)
+            {
+                rightIdx = middleIdx - 1;
+            }
+            else if (elements[middleIdx] < target)
+            {
+                leftIdx = middleIdx + 1;
+            }
+            else return middleIdx;
+        }
+        return -1; // not found
+    }
 }
